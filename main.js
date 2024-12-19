e14edfd9f808fd9865ab3cf347a7683a6cb9f769
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-12-19 17:28:52 +0000

    fix some errors and make it more responsive for mobile devices

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ let output = document.getElementById("output")
 let run = document.getElementById("run")
 let reset = document.getElementById("reset")
 
+
+
+// Switch between input and output from mobile devices
+let switchBtns = document.querySelectorAll(".switcher > button")
+const MobileSwitcher = ()=>{
+    switchBtns.forEach((switcher)=>{
+        switcher.addEventListener('click' , ()=>{
+            switchBtns.forEach(switcher =>{
+                switcher.classList.remove("active")
+            })
+            if(switcher.id == "switch-inp"){
+                document.getElementById("input-container").style.display = "block"
+                document.getElementById("output-container").style.display = "none"
+            }
+            else if(switcher.id == "switch-out"){
+                document.getElementById("input-container").style.display = "none"
+                document.getElementById("output-container").style.display = "block"
+            }
+            switcher.classList.add("active")
+        })
+    })
+}
+MobileSwitcher()
+
+
+
+
 // save original console
 let originalConsole = console.log
 input.value = 
@@ -13,30 +40,35 @@ console.log("hello world")
 console.log = (...messages)=>{
     messages.forEach(message =>{
         let codeLIne = document.createElement("div")
+        if(typeof message == "object"){ 
+            if (message == null){
+                codeLIne.style.color = "grey"
+            }
+            codeLIne.textContent = JSON.stringify(message, null, 2)
+        }
+        else {
+            if (typeof message == "undefined"){
+                codeLIne.style.color = "grey"
+                codeLIne.innerHTML = undefined
+            }
+            else {
+                typeof message == "number" ? codeLIne.style.color = "#30e8ff" 
+                : typeof message == "boolean" ? codeLIne.style.color = "#ce37ce" 
+                : codeLIne.style.color = "#fff"
 
-        typeof message == "number" ? codeLIne.style.color = "#00b8ff"
-        : typeof message == "boolean" ?  codeLIne.style.color = "#bd31ff"
-        : typeof message == "undefined" ?  codeLIne.style.color = "grey" 
-        : typeof message == "null" ?  codeLIne.style.color = "grey" 
-        : codeLIne.style.color = "#fff"
-        // better output for object
-        codeLIne.innerHTML = 
-        typeof message === "object"? 
-            JSON.stringify(message, null, 2) 
-        : typeof message == "null" ? "null"
-        :message
+
+                codeLIne.textContent = message
+            }
+            
+        }
         output.appendChild(codeLIne)
     })
     originalConsole.apply(console , messages)
 }
 
-
-document.write = (...messages)=>{
-    messages.forEach(message =>{
-        let codeLIne = document.createElement("div")
-        codeLIne.innerHTML = message
-        output.appendChild(codeLIne)
-    })
+// disable document.write methode
+document.write = ()=>{
+    alert("document.write can't use here")
 }
 
 // Run code
@@ -52,18 +84,21 @@ const RunCode = ()=>{
         errorLine.style.color = "red"
         output.appendChild(errorLine)
     }
-    
 }
-
+// Reset to initial code 
 const ResetCode = ()=>{
     input.value = 
 `// This a js code
 console.log("hello world")` 
     output.innerHTML = ""
 }
+
+// on click events (run & reset)
 run.addEventListener("click" , RunCode)
 reset.addEventListener("click" , ResetCode)
 
 
 // Get current date 
 document.getElementById("date").innerHTML = new Date().getFullYear()
+
+
